56ddf5bad717b0f34952155aac0871a94669a57e
Andy Vo <andyqvo@gmail.com>
2021-08-13 17:14:59 -0700

    init k6

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export let options = {
+  stages: [
+    { duration: '20s', target: 25 },
+    { duration: '20s', target: 25 },
+    { duration: '20s', target: 0 },
+  ],
+};
+
 export default function() {
   const randomNumber = Math.floor(Math.random() * 90000);
   http.get(`http://localhost:3000/api/qa/questions/?product_id=${randomNumber}&count=5`);
+  sleep(1);
+  http.get(`http://localhost:3000/api/qa/questions/${randomNumber}/answers/`);
   sleep(1);
 }
