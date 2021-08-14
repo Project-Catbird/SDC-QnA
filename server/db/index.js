4b3767e94dd692074d5710ca68634ac34b208529
Andy Vo <=>
2021-08-14 21:52:59 +0000

    Fixed db URL

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ const { database, username, password } = require('./config/config');
 
 const pool = new Pool({
   user: username,
-  host: '172.20.0.2',
+  host: 'database',
   database,
   password,
   port: 5432
